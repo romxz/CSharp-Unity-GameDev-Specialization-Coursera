95d0c8fd7224f3ef40a3693d6b4050390000a484
rromeroa1 <andreiromalv@gmail.com>
2019-09-28 11:47:05 -0400

    In >> Initialize ConsoleApp1 code, from the first console app lecture Out << Initializing ConsoleApp1 code, from the first console app lecture

---
+++ Intro_to_C#_Programming_and_Unity/Week_1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// First Console App lecture code
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Prints a message
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Print supportive message
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+        }
+    }
+}
