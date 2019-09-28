115ec55ba22671617944e1e677dfc8ab61d76b9e
rromeroa1 <andreiromalv@gmail.com>
2019-09-28 12:51:08 -0400

    Shortening filepaths

---
+++ Intro_C#_and_Unity/Week_1/FirstUnityScript/Assets/Scripts/PrintRainMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintRainMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello, World!");
+        print("Some message");
+        print("It is raining");
+    }
+
+}
