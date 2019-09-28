c92b26f5431c833a4b27d51c938cc26ab1f6b318
rromeroa1 <andreiromalv@gmail.com>
2019-09-28 14:55:35 -0400

    Refactoring folders
    In >> W1, A1

---
+++ Intro_C#_and_Unity/Week_1/ProgrammingAssignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment1
+{
+    /// <summary>
+    /// Complete your own solution!
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
