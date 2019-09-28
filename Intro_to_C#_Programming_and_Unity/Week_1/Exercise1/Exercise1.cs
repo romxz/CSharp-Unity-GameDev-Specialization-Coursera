86db061ea45dfdf2239f2f1710316ee38f887434
rromeroa1 <andreiromalv@gmail.com>
2019-09-28 11:58:37 -0400

    Completing Exercise1 from Week1

---
+++ Intro_to_C#_Programming_and_Unity/Week_1/Exercise1/Exercise1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Excercise1
+{
+    /// <summary>
+    /// Solution to Exercise 1
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Print the names
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // Problem 1: Output your name
+            Console.WriteLine("My name is: Myname");
+            Console.WriteLine();
+
+            // Problem 2: Output your nemesis' name
+            Console.WriteLine("My nemesis is Nemesis");
+            Console.WriteLine();
+        }
+    }
+}
