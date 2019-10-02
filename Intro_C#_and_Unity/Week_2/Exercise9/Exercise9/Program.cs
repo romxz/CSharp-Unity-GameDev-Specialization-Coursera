ac4018d557da313bdf71b053ad3b21cd0c1db365
rromeroa1 <andreiromalv@gmail.com>
2019-10-01 20:23:38 -0400

    create W2E9

---
+++ Intro_C#_and_Unity/Week_2/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{    
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+
+            // tell the deck to shuffle and print itself
+
+            // take the top card from the deck and print the card rank and suit
+
+            // take the top card from the deck and print the card rank and suit
+        }
+    }
+}
