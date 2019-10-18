a8a02b3802add33c51bc3946c7e813382974c71c
romxz <andreiromalv@gmail.com>
2019-10-18 10:11:24 -0400

    out: W1_2_Array_Lists_3_Arrays_in_Practice in: Ex1

--- More_C#_and_Unity/Week_1/2_Arrays_Lists/3_Arrays_in_Practice/Assets/scripts/CharacterChanger.cs
+++ More_C#_and_Unity/Week_1/2_Arrays_Lists/3_Arrays_in_Practice/Assets/scripts/CharacterChanger.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 /// <summary>
 /// Changes character on left mouse button
+/// Uses array to store prefabs, and load them as assets from the Assets/Resoures/prefabs folder
 /// </summary>
 public class CharacterChanger : MonoBehaviour {
-    [SerializeField]
+    //[SerializeField] // No need, loading from Resources
+    GameObject[] prefabCharacters = new GameObject[4];
+
+/*    [SerializeField]
     GameObject prefabCharacter0;
     [SerializeField]
     GameObject prefabCharacter1;
     [SerializeField]
     GameObject prefabCharacter2;
     [SerializeField]
-    GameObject prefabCharacter3;
+    GameObject prefabCharacter3;*/
 
     // need for location of new character
     GameObject currentCharacter;
@@ -25,8 +29,17 @@ public class CharacterChanger : MonoBehaviour {
     /// Use this for initialization
     /// </summary>
     void Start() {
+        // populate array with characters
+        for (int i = 0; i < 4; i++) {
+            prefabCharacters[i] = Resources.Load("prefabs/Character" + i) as GameObject;
+        }
+/*        prefabCharacters[0] = Resources.Load("prefabs/Character0") as GameObject;
+        prefabCharacters[1] = Resources.Load("prefabs/Character1") as GameObject;
+        prefabCharacters[2] = Resources.Load("prefabs/Character2") as GameObject;
+        prefabCharacters[3] = Resources.Load("prefabs/Character3") as GameObject;*/
+
         currentCharacter = Instantiate<GameObject>(
-            prefabCharacter0, Vector3.zero,
+            prefabCharacters[0], Vector3.zero,
             Quaternion.identity);
     }
 
@@ -45,8 +58,10 @@ public class CharacterChanger : MonoBehaviour {
                 Destroy(currentCharacter);
 
                 // instantiate a new random character
-                int prefabNumber = Random.Range(0, 4);
-                if (prefabNumber == 0) {
+                //int prefabNumber = Random.Range(0, 4);
+                currentCharacter = Instantiate(prefabCharacters[Random.Range(0, 4)],
+                        position, Quaternion.identity);
+                /*if (prefabNumber == 0) {
                     currentCharacter = Instantiate(prefabCharacter0,
                         position, Quaternion.identity);
                 } else if (prefabNumber == 1) {
@@ -58,7 +73,7 @@ public class CharacterChanger : MonoBehaviour {
                 } else {
                     currentCharacter = Instantiate(prefabCharacter3,
                         position, Quaternion.identity);
-                }
+                }*/
             }
         } else {
             // no change character input
---
+++ More_C#_and_Unity/Week_1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleCards;
+
+namespace Exercise1
+{
+    /// <summary>
+    /// Exercise 1 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Practices using arrays
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+        }
+    }
+}
