57d41563dfb5eeb010117a81e19a4b669862b907
rromeroa1 <andreiromalv@gmail.com>
2019-10-17 21:42:59 -0400

    out W4 Ex20 - Keyboard Input exercise

---
+++ Intro_C#_and_Unity/Week_4/Exercise20/Assets/scripts/Driver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour {
+    // move support
+    const float MoveUnitsPerSecond = 6;
+
+    // Update is called once per frame
+    void Update() {
+        // move horizontally and vertically
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (horizontalInput != 0 || verticalInput != 0) {
+            Vector3 position = transform.position;
+            position.x += horizontalInput * MoveUnitsPerSecond * Time.deltaTime;
+            position.y += verticalInput * MoveUnitsPerSecond * Time.deltaTime;
+            transform.position = position;
+        }
+    }
+}
