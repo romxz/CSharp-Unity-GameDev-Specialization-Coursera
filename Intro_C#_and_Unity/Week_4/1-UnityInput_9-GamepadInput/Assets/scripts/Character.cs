1cbebf0572fc805b6f929dd88fd178b7e7ba0cab
rromeroa1 <andreiromalv@gmail.com>
2019-10-17 21:49:59 -0400

    out W4 L9 Gamepad Input

---
+++ Intro_C#_and_Unity/Week_4/1-UnityInput_9-GamepadInput/Assets/scripts/Character.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A character
+/// </summary>
+public class Character : MonoBehaviour
+{
+    // saved for efficiency
+    float colliderHalfWidth;
+    float colliderHalfHeight;
+
+    // movement support
+    const float MoveUnitsPerSecond = 5;
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+        // save for efficiency
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        colliderHalfWidth = collider.size.x / 2;
+        colliderHalfHeight = collider.size.y / 2;
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+        // move based on input
+        Vector3 position = transform.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (horizontalInput != 0)
+        {
+            position.x += horizontalInput * MoveUnitsPerSecond *
+                Time.deltaTime;
+        }
+        if (verticalInput != 0)
+        {
+            position.y += verticalInput * MoveUnitsPerSecond *
+                Time.deltaTime;
+        }
+
+        // move character to new position and clamp in screen
+        transform.position = position;
+        ClampInScreen();
+	}
+
+    /// <summary>
+    /// Clamps the character in the screen
+    /// </summary>
+    void ClampInScreen()
+    {
+        // clamp position as necessary
+        Vector3 position = transform.position;
+        if (position.x - colliderHalfWidth < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + colliderHalfWidth;
+        }
+        else if (position.x + colliderHalfWidth > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenRight - colliderHalfWidth;
+        }
+        if (position.y + colliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenTop - colliderHalfHeight;
+        }
+        else if (position.y - colliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenBottom + colliderHalfHeight;
+        }
+        transform.position = position;
+    }
+}
---
+++ Intro_C#_and_Unity/Week_4/1-UnityInput_9-GamepadInput/Assets/scripts/CharacterChanger.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Changes character on left mouse button
+/// </summary>
+public class CharacterChanger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefabCharacter0;
+    [SerializeField]
+    GameObject prefabCharacter1;
+    [SerializeField]
+    GameObject prefabCharacter2;
+    [SerializeField]
+    GameObject prefabCharacter3;
+
+    // need for location of new character
+    GameObject currentCharacter;
+
+    // first frame input support
+    bool previousFrameChangeCharacterInput = false;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        currentCharacter = Instantiate<GameObject>(
+            prefabCharacter0, Vector3.zero,
+            Quaternion.identity);
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // change character on left mouse button
+        if (Input.GetAxis("ChangeCharacter") > 0)
+        {
+            // only change character on first input frame
+            if (!previousFrameChangeCharacterInput)
+            {
+                previousFrameChangeCharacterInput = true;
+
+                // save current position and destroy current character
+                Vector3 position = currentCharacter.transform.position;
+                Destroy(currentCharacter);
+
+                // instantiate a new random character
+                int prefabNumber = Random.Range(0, 4);
+                if (prefabNumber == 0)
+                {
+                    currentCharacter = Instantiate(prefabCharacter0, 
+                        position, Quaternion.identity);
+                }
+                else if (prefabNumber == 1)
+                {
+                    currentCharacter = Instantiate(prefabCharacter1, 
+                        position, Quaternion.identity);
+                }
+                else if (prefabNumber == 2)
+                {
+                    currentCharacter = Instantiate(prefabCharacter2, 
+                        position, Quaternion.identity);
+                }
+                else
+                {
+                    currentCharacter = Instantiate(prefabCharacter3, 
+                        position, Quaternion.identity);
+                }
+            }
+        }
+        else
+        {
+            // no change character input
+            previousFrameChangeCharacterInput = false;
+        }
+    }
+}
