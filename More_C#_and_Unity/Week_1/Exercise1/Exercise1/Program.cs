1e5055934dd39b8a7f0eedf6623f8841b8248d8e
romxz <andreiromalv@gmail.com>
2019-10-18 10:21:38 -0400

    out: Ex1

--- More_C#_and_Unity/Week_1/Exercise1/Exercise1/Program.cs
+++ More_C#_and_Unity/Week_1/Exercise1/Exercise1/Program.cs
@@ -5,21 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using ConsoleCards;
 
-namespace Exercise1
-{
+namespace Exercise1 {
     /// <summary>
     /// Exercise 1 solution
     /// </summary>
-    class Program
-    {
+    class Program {
         /// <summary>
         /// Practices using arrays
         /// </summary>
         /// <param name="args">command-line arguments</param>
-        static void Main(string[] args)
-        {
-
-            Console.WriteLine();
+        static void Main(string[] args) {
+            // Problem 1
+            Deck deck = new Deck();
+            Card[] cards = new Card[5];
+            deck.Shuffle();
+            // Problem 2
+            cards[0] = deck.TakeTopCard();
+            cards[0].FlipOver();
+            cards[0].Print();
+            // Problem 3
+            cards[1] = deck.TakeTopCard();
+            cards[1].FlipOver();
+            for (int i = 0; i < 2; i++) {
+                cards[i].Print();
+            }
+            //Console.WriteLine();
         }
     }
 }
