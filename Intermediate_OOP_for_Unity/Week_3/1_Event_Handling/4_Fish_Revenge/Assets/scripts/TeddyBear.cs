50be3ceacb6c5bfee8bf69e549939baba74b2f09
romxz <andreiromalv@gmail.com>
2019-10-29 21:58:21 -0400

    in: W3.S1.L4: adding event handler

--- Intermediate_OOP_for_Unity/Week_3/1_Event_Handling/4_Fish_Revenge/Assets/scripts/TeddyBear.cs
+++ Intermediate_OOP_for_Unity/Week_3/1_Event_Handling/4_Fish_Revenge/Assets/scripts/TeddyBear.cs
@@ -5,23 +5,21 @@ using UnityEngine;
 /// <summary>
 /// A teddy bear
 /// </summary>
-public class TeddyBear : MonoBehaviour
-{
-	[SerializeField]
-	float minImpulseForce;
+public class TeddyBear : MonoBehaviour {
+    [SerializeField]
+    float minImpulseForce;
     [SerializeField]
     float maxImpulseForce;
 
-	// death support
-	const float TeddyBearLifespanSeconds = 10;
-	Timer deathTimer;
+    // death support
+    const float TeddyBearLifespanSeconds = 10;
+    Timer deathTimer;
 
     /// <summary>
     /// Use this for initialization
     /// </summary>
-    void Start()
-    {		
-		// apply impulse force to get teddy bear moving
+    void Start() {
+        // apply impulse force to get teddy bear moving
         float angle = Random.Range(0, 2 * Mathf.PI);
         Vector2 direction = new Vector2(
             Mathf.Cos(angle), Mathf.Sin(angle));
@@ -29,22 +27,20 @@ public class TeddyBear : MonoBehaviour
         GetComponent<Rigidbody2D>().AddForce(
             direction * magnitude,
             ForceMode2D.Impulse);
-		
-		// create and start timer
-		deathTimer = gameObject.AddComponent<Timer>();
-		deathTimer.Duration = TeddyBearLifespanSeconds;
-		deathTimer.Run();
-	}
+
+        // create and start timer
+        deathTimer = gameObject.AddComponent<Timer>();
+        deathTimer.Duration = TeddyBearLifespanSeconds;
+        deathTimer.Run();
+    }
 
     /// <summary>
     /// Update is called once per frame
     /// </summary>
-    void Update()
-    {		
-		// kill teddy bear if death timer finished
-		if (deathTimer.Finished)
-        {
-			Destroy(gameObject);
-		}
-	}
+    void Update() {
+        // kill teddy bear if death timer finished
+        if (deathTimer.Finished) {
+            Destroy(gameObject);
+        }
+    }
 }
