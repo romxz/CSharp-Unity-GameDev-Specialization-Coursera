9b935e64b0dc041ed76df2ff95cb5d13176afb89
romxz <andreiromalv@gmail.com>
2019-10-29 21:12:37 -0400

    out: PA2 in: W3.S1:EventHandling

--- Intermediate_OOP_for_Unity/Week_3/1_Event_Handling/4_Fish_Revenge/Assets/scripts/HUD.cs
+++ Intermediate_OOP_for_Unity/Week_3/1_Event_Handling/4_Fish_Revenge/Assets/scripts/HUD.cs
@@ -6,29 +6,26 @@ using UnityEngine.UI;
 /// <summary>
 /// The HUD
 /// </summary>
-public class HUD : MonoBehaviour
-{
+public class HUD : MonoBehaviour {
     // score support
     static Text scoreText;
     static int score = 0;
     const string ScorePrefix = "Score: ";
 
-	/// <summary>
-	/// Use this for initialization
-	/// </summary>
-	void Start()
-	{
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start() {
         // initialize score text
         scoreText = GameObject.FindGameObjectWithTag("ScoreText").GetComponent<Text>();
-        scoreText.text = ScorePrefix + score;		
-	}
+        scoreText.text = ScorePrefix + score;
+    }
 
     /// <summary>
     /// Updates the score
     /// </summary>
     /// <param name="points">points to add</param>
-    public static void AddPoints(int points)
-    {
+    public static void AddPoints(int points) {
         score += points;
         scoreText.text = ScorePrefix + score;
     }
