0d1af493e3826568c9ca41343fedaa6eabbb07dc
romxz <andreiromalv@gmail.com>
2019-10-29 22:58:35 -0400

    out: W3.S1.L5: Adding an event manager in: W3.S1.Ex9 Creating an event manager

---
+++ Intermediate_OOP_for_Unity/Week_3/1_Event_Handling/5_Adding_Event_Manager/Assets/scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class EventManager {
+    static Fish invoker;
+    static UnityAction<int> listener;
+
+    /// <summary>
+    /// Adds the invoker
+    /// </summary>
+    /// <param name="script"></param>
+    public static void AddInvoker(Fish script) {
+        invoker = script;
+        if (listener != null) {
+            invoker.AddPointsAddedEventListener(listener);
+        }
+    }
+
+    public static void AddListener(UnityAction<int> handler) {
+        listener = handler;
+        if (invoker != null) {
+            invoker.AddPointsAddedEventListener(listener);
+        }
+    }
+}
